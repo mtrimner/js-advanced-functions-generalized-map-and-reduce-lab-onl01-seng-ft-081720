4c0d096901451eea465924aac23ba30bbe5405ce
mtrimner <matt.trimner@gmail.com>
2020-12-01 14:45:49 -0800

    commit tree a5383f2b04e4f2fb618b2d75eea25181568f28ce

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your functions here
+
+
+function map(array, cb){
+    let newArray = []
+    for (let i = 0; i < array.length; i++) {
+    newArray.push(cb(array[i]))
+    }
+    return newArray
+}
+
+function reduce(array, cb, starting=null){
+    let answer;
+    let i
+    if (starting === null) {
+        answer = array[0]
+        i = 1
+    } else {
+        answer = starting
+        i = 0
+    }
+    for (i; i < array.length; i++) {
+       answer = cb(array[i], answer)
+    }
+    return answer
+}
